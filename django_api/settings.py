04158a5731f80aa71a1a16f00c0beff121200736
Xiaoxi <yxx0827@gmail.com>
2020-04-08 22:03:43 -0400

    add static folder

--- django_api/settings.py
+++ django_api/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv("DEBUG") == 'True'
 
 ALLOWED_HOSTS = [
     'django-api-xiaoxihome.herokuapp.com',
-    '127.0.0.1',
+    '127.0.0.1:8000',
     '*.postwoman.io'
 ]
 
@@ -178,3 +178,36 @@ SIMPLE_JWT = {
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
