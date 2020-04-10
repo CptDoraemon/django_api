2618fedeec7d435c0ce1b718114b24df73d9676a
Xiaoxi <yxx0827@gmail.com>
2020-04-10 02:16:00 -0400

    cors

--- django_api/settings.py
+++ django_api/settings.py
@@ -31,8 +31,6 @@ DEBUG = os.getenv("DEBUG") == 'True'
 ALLOWED_HOSTS = [
     'django-api-xiaoxihome.herokuapp.com',
     '127.0.0.1:8000',
-    '*.postwoman.io',
-    'localhost:3000'
 ]
 
 
@@ -46,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     # 'tutorial_snippets.apps.SnippetsConfig',
     'posts.apps.PostsConfig',
     'account',
@@ -56,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -214,3 +214,10 @@ if os.getenv("DEBUG") != 'True':
             },
         }
     }
+
+CORS_ORIGIN_WHITELIST = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:3000",
+    "https://cptdoraemon.github.io"
+]
