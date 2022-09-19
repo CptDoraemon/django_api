a41880f074b380875833a67ef3e5f7a132da61aa
Xiaoxi <yxx0827@gmail.com>
2022-09-19 18:14:48 +0000

    migrate to planetscale mysql - ./django_psdb_engine is their custom DB wrapper tips for future migration: - django-admin dumpdata - python manage.py does the same as django-admin, but save you the trouble to set up env vars - disable start_up hooks if you have trouble to connect to new DB (table not exists)

--- django_api/settings.py
+++ django_api/settings.py
@@ -91,11 +91,15 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default':  dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
+  'default': {
+    'ENGINE': 'django_psdb_engine',
+    'NAME': os.environ.get('DB_NAME'),
+    'HOST': os.environ.get('DB_HOST'),
+    'PORT': os.environ.get('DB_PORT'),
+    'USER': os.environ.get('DB_USER'),
+    'PASSWORD': os.environ.get('DB_PASSWORD'),
+    'OPTIONS': {'ssl': {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}}
+  }
 }
 
 
--- start_up/apps.py
+++ start_up/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class StartUpConfig(AppConfig):
     name = 'start_up'
 
-    def ready(self):
-        # do a query to database to accelerate first connection after app starts
-        from posts.models import Post
-        print(f'App ready: {Post.objects.all().count()} posts in total.')
+    # def ready(self):
+    #     # do a query to database to accelerate first connection after app starts
+    #     from posts.models import Post
+    #     print(f'App ready: {Post.objects.all().count()} posts in total.')
